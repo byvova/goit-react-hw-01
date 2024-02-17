12446d7cf8344301465ddbeb08a3f89510cbe760
byvova <>
2024-02-17 22:54:36 +0200

    asasas

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,32 +1,24 @@
-import css from './TransactionHistory.module.css'
+import css from './TransactionHistory.module.css';
 
-export const TransactionHistory = (items) => {
-    console.log();
+export const TransactionHistory = ({ items }) => {
     return (
-        <>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th className={css.header}>Type</th>
-                        <th className={css.header}>Amount</th>
-                        <th className={css.header}>Currency</th>
+        <table>
+            <thead>
+                <tr>
+                    <th className={css.header}>Type</th>
+                    <th className={css.header}>Amount</th>
+                    <th className={css.header}>Currency</th>
+                </tr>
+            </thead>
+            <tbody>
+                {items.map((tran) => (
+                    <tr key={tran.id}>
+                        <td className={css.white}>{tran.type}</td>
+                        <td className={css.white}>{tran.amount}</td>
+                        <td className={css.white}>{tran.currency}</td>
                     </tr>
-                </thead>
-                {items.items.map((tran) => {
-                    return (
-                        <tbody key={tran.id}>
-                            <tr>
-                                <td className={css.white}>{tran.type}</td>
-                                <td className={css.white}>{tran.amount}</td>
-                                <td className={css.white}>{tran.currency}</td>
-                            </tr>
-                        </tbody>
-                    )
-                })}
-            </table>
-
-
-        </>
-    )
-}
+                ))}
+            </tbody>
+        </table>
+    );
+};
